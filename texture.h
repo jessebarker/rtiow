c059a625b5993a6de01785a1f329cbb1bcc14c8b
Jesse Barker <jesseb@unity3d.com>
2020-03-06 08:01:37 -0800

    Clean up Texture::value interface

--- texture.h
+++ texture.h
@@ -6,7 +6,7 @@
 class Texture
 {
 public:
-    virtual vec3 value(float u, float v, const vec3& point) const = 0;
+    virtual vec3 value(const vec2& uv, const vec3& point) const = 0;
 };
 
 class ConstantTexture : public Texture
@@ -17,7 +17,7 @@ public:
     ConstantTexture(const vec3& c) : color_(c)
     {
     }
-    virtual vec3 value(float u, float v, const vec3& point) const
+    virtual vec3 value(const vec2& uv, const vec3& point) const
     {
         return color_;
     }
@@ -34,14 +34,14 @@ public:
         , even_(even)
     {
     }
-    virtual vec3 value(float u, float v, const vec3& point) const
+    virtual vec3 value(const vec2& uv, const vec3& point) const
     {
         float sines = sin(10.0f * point.x()) * sin(10.0f * point.y()) * sin(10.0f * point.z());
         if (sines < 0.0f)
         {
-            return odd_->value(u, v, point);
+            return odd_->value(uv, point);
         }
-        return even_->value(u, v, point);
+        return even_->value(uv, point);
     }
 };
 
@@ -53,7 +53,7 @@ public:
         : perlin_(scale)
     {
     }
-    virtual vec3 value(float u, float v, const vec3& point) const
+    virtual vec3 value(const vec2& uv, const vec3& point) const
     {
         //return vec3(1.0f) * perlin_.noise(point);
         //return vec3(1.0f) * 0.5f * (1.0f + sin(perlin_.scale() * point.z() + 10.0f * perlin_.turbulence(point)));
@@ -72,12 +72,12 @@ public:
     {
         image_.load(filename);
     }
-    virtual vec3 value(float u, float v, const vec3& point) const
+    virtual vec3 value(const vec2& uv, const vec3& point) const
     {
         vec2 dimensions(image_.dimensions().x(), image_.dimensions().y());
-        vec2 uv(u, 1.0f - v);
-        uv *= dimensions;
-        uvec2 xy(uv.x(), uv.y());
+        vec2 uvVflip(uv.x(), 1.0f - uv.y());
+        uvVflip *= dimensions;
+        uvec2 xy(uvVflip.x(), uvVflip.y());
         unsigned char* curPixel = image_.pixels() + (xy.y() * image_.dimensions().x() + xy.x()) * image_.components();
         vec3 result(curPixel[0], curPixel[1], curPixel[2]);
         result /= 255.0f;
